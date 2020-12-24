06a4a347bb40f60d8abb556a770a88bf7dd1a643
Gabriel Morales <gabrielmorales1@pm.me>
2020-12-24 08:40:56 -0500

    Newton's Method [Complete]

---
+++ Section 2/2.3 Newton_s method/newton.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f(double x){
+	/* === Define the fuction to use ===*/
+	return cos(x) - x;
+}
+double fp(double x){
+	/* === Define the derivative of the function f(x) */
+	return -sin(x) - 1;
+}
+
+void Newton(int n_0, double p_0, double tol){
+	double p;
+
+	int i = 1;
+	while (i <= n_0){
+		p = p_0 - f(p_0) / fp(p_0);
+		if (fabs(p - p_0) < tol){
+			printf("Result: %lf\n", p);
+			return;
+		}
+		i++;
+		p_0 = p;
+	}
+	return;
+}
+
+int main(){
+	/* === INSERT DATA HERE === */
+	int n_0 = 100;
+	double p_0 = 0;
+	double tol = pow(10, -6);
+
+	/* === Calculate the Newton's Method === */
+	Newton(n_0, p_0, tol);
+
+	return 0;
+}
