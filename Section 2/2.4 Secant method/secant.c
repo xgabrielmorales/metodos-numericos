e12567caf19ba5b113c1b2e904087db69f3e506d
Gabriel Morales <gabrielmorales1@pm.me>
2021-01-01 09:47:56 -0500

    Secant Method [Complete] [Matlab] [C]

--- Section 2/2.4 Secant method/secant.c
+++ Section 2/2.4 Secant method/secant.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 double f(double x){
-	/* === Define the fuction to use ===*/
-	return cos(x) - x;
+	/* === DEFINE THE FUCTION TO USE ===*/
+	return 0;
 }
 
-void Secant(double p_0, double p_1, int n, double tol){
+double Secant(double p_0, double p_1, int n, double tol){
 	double p;
 	int i = 2;
 
@@ -17,10 +16,8 @@ void Secant(double p_0, double p_1, int n, double tol){
 	while (i < n){
 		p = p_1 - (q_1 * (p_1 - p_0) / q_1 - q_0);
 
-		if (fabs(p - p_1) < tol){
-			printf("%.7f\n", p);
-			exit(0);
-		}
+		if (fabs(p - p_1) < tol)
+			return p;
 
 		i++;
 		p_0 = p_1;
@@ -29,20 +26,19 @@ void Secant(double p_0, double p_1, int n, double tol){
 		q_0 = q_1;
 		q_1 = f(p);
 	}
-
 	printf("The method failed after %d iterations.\n", n);
-	return;
 }
 
 int main(){
 	/* === INSERT DATA HERE === */
-	int n = 100;
-	double p_0 = 0.5;
-	double p_1 = 1;
-	double tol = pow(10, -6);
+	int n;
+	double p_0;
+	double p_1;
+	double tol;
 
-	/* === Calculate the secant === */
-	Secant(p_0, p_1, n, tol);
+	/* === CALCULATE THE SECANT === */
+	double r = Secant(p_0, p_1, n, tol);
+	printf("%lf\n", r);
 
 	return 0;
 }
---
+++ Section 2/2.4 Secant method/secant.m
@@ -0,0 +1,33 @@
+# SECANT METHOD
+
+# Input
+# f(x):     Function
+# p_0, p_1: Initial approximations
+# n:        Maximum number of iterations
+# tol:      Maximum tolerated error
+
+# Output
+# Approximate solution p or message of failure.
+
+function r = secant(f, p_0, p_1, n, tol)
+	q_0 = f(p_0);
+	q_1 = f(p_1);
+
+	i = 2;
+	while i<n
+		p = p_1 - (q_1 * (p_1 - p_0) / (q_1 - q_0));
+
+		if abs(p - p_1) < tol
+			r = p;
+			return;
+		endif
+
+		i++;
+		p_0 = p_1;
+		p_1 = p;
+
+		q_0 = q_1;
+		q_1 = f(p);
+	endwhile
+	printf("The method failed after %d iterations.\n", n);
+endfunction
